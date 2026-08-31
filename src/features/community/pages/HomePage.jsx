1c233eaa57a2c107cc6d60adeeef7312ed6be13d
MJ <vovodla74@gmail.com>
2026-08-31 14:51:35 +0900

    merge: main 병합 충돌 해결 (HomePage, AppRouter)

--- src/features/community/pages/HomePage.jsx
+++ src/features/community/pages/HomePage.jsx
@@ -9,8 +9,6 @@ const HomePage = () => {
             <Link to="/signup">회원가입</Link>
             <Link to="/login">로그인</Link>   
             <Link to="/board">커뮤니티</Link>
-            <Link to="/signup"> 회원가입 </Link>
-            <Link to="/login"> 로그인 </Link>   
             <Link to="/accommodation/1"> 숙소 상세 페이지 </Link>
             <Link to="/accommodation/1/review/new"> 숙소 리뷰 작성 페이지 </Link>
             <Link to="/accommodation/1/review/1/edit"> 숙소 리뷰 수정 페이지 </Link>
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,15 +2,9 @@ import LoginPage from '@/features/auth/pages/LoginPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '@/features/community/pages/HomePage';
 import SignupPage from '@/features/auth/pages/SignupPage';
-import AccommodationDetailPage from '../features/accommodation/pages/AccommodationDetailPage';
-import AccommodationReviewCreatePage from '../features/accommodation/pages/AccommodationReviewCreatePage';
-import CartPage from '../features/accommodation/pages/CartPage';
-import BookingListPage from '../features/accommodation/pages/BookingListPage';
-import BookingDetailPage from '../features/accommodation/pages/BookingDetailPage';
-import AccommodationRecommendPage from '../features/accommodation/pages/AccommodationRecommendPage';
 import OAuthSuccessPage from '@/features/auth/pages/OAuthSuccessPage';
 import Header from '@/components/Header';
-import MyPage from '@/features/auth/pages/MyPage'; 
+import MyPage from '@/features/auth/pages/MyPage';
 
 import Place from '@/features/place/Place';
 import Planner from '@/features/place/Planner';
@@ -18,15 +12,35 @@ import BoardListPage from '@/features/community/pages/BoardListPage';
 import BoardWritePage from '@/features/community/pages/BoardWritePage';
 import BoardDetailPage from '../features/community/pages/BoardDetailPage';
 
+import AccommodationDetailPage from '../features/accommodation/pages/AccommodationDetailPage';
+import AccommodationReviewCreatePage from '../features/accommodation/pages/AccommodationReviewCreatePage';
+import CartPage from '../features/accommodation/pages/CartPage';
+import BookingListPage from '../features/accommodation/pages/BookingListPage';
+import BookingDetailPage from '../features/accommodation/pages/BookingDetailPage';
+import AccommodationRecommendPage from '../features/accommodation/pages/AccommodationRecommendPage';
 
 const AppRouter = () => {
     return (
-       <BrowserRouter> 
+       <BrowserRouter>
        <Header/>
         <Routes>
             <Route path="/signup" element={<SignupPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="/oauth2/success" element={<OAuthSuccessPage/>}/>
+            <Route path="/mypage" element={<MyPage/>}/>
+            <Route path="/board" element={<BoardListPage/>}/>
+            <Route path="/board/write" element={<BoardWritePage/>}/>
+            <Route path="/board/:boardId" element={<BoardDetailPage/>}/>
+            <Route path="/place" element={<Place />} />
+            <Route path="/planner" element={<Planner />} />
+            <Route path="/accommodation/:accommodationId" element={<AccommodationDetailPage/>}/>
+            <Route path="/accommodation/:accommodationId/review/new" element={<AccommodationReviewCreatePage/>}/>
+            <Route path="/accommodation/:accommodationId/review/:reviewId/edit" element={<AccommodationReviewCreatePage/>}/>
+            <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/bookings" element={<BookingListPage/>}/>
+            <Route path="/bookings/:bookingId" element={<BookingDetailPage/>}/>
+            <Route path="/recommend" element={<AccommodationRecommendPage/>}/>
         </Routes>
        </BrowserRouter>
     );
